936eb7b8f421ad94729f6c02257dbb504dcde6ad
sailorv <sailorv@gmail.com>
2020-10-07 01:22:42 -0500

    Updates

---
+++ precache-manifest.a3db97141e97a547ba872762b2e8e53d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34bcea25d14e731d20c30c12a3461c06",
+    "url": "/moon-calendar/index.html"
+  },
+  {
+    "revision": "2a8ca631c538c43d8614",
+    "url": "/moon-calendar/static/css/main.0b8eb840.chunk.css"
+  },
+  {
+    "revision": "155d99856246063fc41f",
+    "url": "/moon-calendar/static/js/2.f12bc997.chunk.js"
+  },
+  {
+    "revision": "849657740f66d00720db130cc1e94c97",
+    "url": "/moon-calendar/static/js/2.f12bc997.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a8ca631c538c43d8614",
+    "url": "/moon-calendar/static/js/main.ac39d31f.chunk.js"
+  },
+  {
+    "revision": "5b988f4fed3581f39c9c",
+    "url": "/moon-calendar/static/js/runtime-main.8ad4c45a.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/moon-calendar/precache-manifest.a3db97141e97a547ba872762b2e8e53d.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/moon-calendar/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
